e939afc03bf9f5fa4499b0929be0d452c7da5fc0
Reed Aldrich <reed.aldrich@gmail.com>
2017-04-10 11:32:44 +0900

    code clean up

--- ps3_hangman.py
+++ ps3_hangman.py
@@ -10,8 +10,6 @@ def loadWords():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
     
-    Depending on the size of the word list, this function may
-    take a while to finish.
     """
     print("Loading word list from file...")
     inFile = open(WORDLIST_FILENAME, 'r')
